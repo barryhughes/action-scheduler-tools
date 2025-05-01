0a885b09f4e6272df83c3e0f52a44356444fefa8
barryhughes <3594411+barryhughes@users.noreply.github.com>
2025-04-30 19:59:25 -0700

    Cross-version support for uniqueness when auto-grouping.

--- php/Autogrouper.php
+++ php/Autogrouper.php
@@ -2,10 +2,6 @@
 
 namespace Automattic\Chronos\Action_Scheduler_Tools;
 
-/**
- * @see https://github.com/woocommerce/action-scheduler/pull/1265
- *      Autogrouping will be unaffective for anything except as_enqueue_async_action() calls until this is addressed.
- */
 class Autogrouper {
 	private const AS_ENQUEUE_FUNCTIONS = [
 		'as_enqueue_async_action',
@@ -21,23 +17,62 @@ class Autogrouper {
 		add_filter( 'pre_as_schedule_cron_action',      [ $this, 'autogroup_cron_action' ], 10, 8 );
 	}
 
-	public function autogroup_async_action( $short_circuit, $hook, $args, $group, $priority, $unique ) {
+	public function autogroup_async_action( $short_circuit, $hook, $args, $group, $priority, $unique = null ) {
+		$unique = $unique === null ? $this->discover_if_unique() : $unique;
 		return $this->autogroup_action( $short_circuit, 'as_enqueue_async_action', $hook, $args, $group, $priority, $unique );
 	}
 
-	public function autogroup_single_action( $short_circuit, $timestamp, $hook, $args, $group, $priority, $unique ) {
+	public function autogroup_single_action( $short_circuit, $timestamp, $hook, $args, $group, $priority, $unique = null ) {
+		$unique = $unique === null ? $this->discover_if_unique() : $unique;
 		return $this->autogroup_action( $short_circuit, 'as_schedule_single_action', $hook, $args, $group, $priority, $unique, $timestamp );
 	}
 
-	public function autogroup_recurring_action( $short_circuit, $timestamp, $interval_in_seconds, $hook, $args, $group, $priority, $unique ) {
+	public function autogroup_recurring_action( $short_circuit, $timestamp, $interval_in_seconds, $hook, $args, $group, $priority, $unique = null ) {
+		$unique = $unique === null ? $this->discover_if_unique() : $unique;
 		return $this->autogroup_action( $short_circuit, 'as_schedule_recurring_action', $hook, $args, $group, $priority, $unique, $timestamp, $interval_in_seconds );
 	}
 
-	public function autogroup_cron_action( $short_circuit, $timestamp, $schedule, $hook, $args, $group, $priority, $unique ) {
+	public function autogroup_cron_action( $short_circuit, $timestamp, $schedule, $hook, $args, $group, $priority, $unique = null ) {
+		$unique = $unique === null ? $this->discover_if_unique() : $unique;
 		return $this->autogroup_action( $short_circuit, 'as_schedule_cron_action', $hook, $args, $group, $priority, $unique, $timestamp, $schedule );
 	}
 
-	private function autogroup_action( $short_circuit, string $function, $hook, $args, $group, $priority, $unique, $timestamp = null, $scheduling = null ) {
+	/**
+	 * The hooks we are using have not always supplied information about uniqueness (and,
+	 * we don't need to go back to many versions to find a point where uniqueness wasn't
+	 * supported). This method helps us support the concept in a cross-version way.
+	 *
+	 * @see https://github.com/woocommerce/action-scheduler/pull/1265
+	 */
+	private function discover_if_unique(): bool {
+		$level = 0;
+
+		foreach ( debug_backtrace() as $frame ) {
+			if ( ++$level > 10 ) {
+				return false;
+			}
+
+			if ( ! isset( $frame['function'] ) || ! in_array( $frame['function'], self::AS_ENQUEUE_FUNCTIONS, true ) ) {
+				continue;
+			}
+
+			switch ( $frame['function'] ) {
+				case 'as_enqueue_async_action':
+					return $frame['args'][3] ?? false;
+
+				case 'as_schedule_cron_action':
+				case 'as_schedule_recurring_action':
+					return $frame['args'][5] ?? false;
+
+				case 'as_schedule_single_action':
+					return $frame['args'][4] ?? false;
+			};
+		}
+
+		return false;
+	}
+
+	private function autogroup_action( $short_circuit, string $function, $hook, $args, $group, $priority, $unique = null, $timestamp = null, $scheduling = null ) {
 		static $lock = false;
 
 		if ( $lock === true || ! empty( $group ) ) {
